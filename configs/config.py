bb09495f3a830ecb742913bd9f6b713353773799
Awynimantha <awynimantha@gmail.com>
2024-12-31 10:41:14 +0530

    refactor modules

--- configs/config.py
+++ configs/config.py
@@ -1,4 +1,5 @@
 import os
+from absl import logging
 
 #dataset info
 BBC_DATASET_PATH = os.path.join('..', 'datasets', 'raw') 
@@ -9,5 +10,4 @@ PIPELINE_ROOT = os.path.join('..' , 'pipeline')
 METADATA_PATH = os.path.join('..', 'metadata', 'metadata.db')
 SERVING_MODEL_DIR = os.path.join('..','model', 'serving_model')
 
-from absl import logging
 logging.set_verbosity(logging.INFO)
